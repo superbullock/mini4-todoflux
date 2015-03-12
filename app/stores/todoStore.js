8452b804f378b9fc7bec6b5bdb9041239afbf037
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-03-12 12:19:42 -0600

    included readme etc

--- app/stores/todoStore.js
+++ app/stores/todoStore.js
@@ -1,8 +1,8 @@
 /**
  * Created by danielbullock on 3/12/15.
  */
-var AppDispatcher = require('./dispatcher/AppDispatcher');
-var appConstants = require('./constants/appConstants');
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var appConstants = require('../constants/appConstants');
 var EventEmitter = require('events').EventEmitter;
 var objectAssign = ('react/lib/Object.assign');
 
